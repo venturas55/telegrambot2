0563c559efd20801a6c138aac3fdb3e5beaf3646
venturas55 <venturas55@hotmail.com>
2026-04-21 12:42:19 +0200

    comentarios

--- bot/middlewares/auth.js
+++ bot/middlewares/auth.js
@@ -6,11 +6,11 @@ export const checkAccess = async (bot, msg, next) => {
   const first_name = msg.from.first_name;
   const last_name = msg.from.last_name;
   const chatId = msg.chat.id;
-  console.log("MSG:",msg);
+  //console.log("MSG:",msg);
 
   try {
     const user = await getUserByTelegramId(userId);
-    console.log(user);
+    //console.log(user);
     if (!user) {
       await bot.sendMessage(chatId, `❌ ${first_name} no estas registrado`);
       await bot.sendMessage(ADMIN_ID, ` ⚠️ El usuario ${first_name} ${last_name} con el id: ${userId} quiere tener acceso al Buscaviento_bot ⚠️`);
