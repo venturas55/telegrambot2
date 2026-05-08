2d8852ff8dc6150f87d1caec913e170797137410
venturas55 <venturas55@hotmail.com>
2026-05-08 13:44:03 +0200

    ecosystem

--- bot/ecosystem.config.js
+++ bot/ecosystem.config.js
@@ -1,9 +1,9 @@
-export default {
+module.exports = {
   apps: [
     {
       name: "telegrambot",
       script: "./index.js",
-      node_args: "--security-revert=CVE-2023-46809",
+      //node_args: "--security-revert=CVE-2023-46809",
       watch: false,
       restart_delay: 3000,
       env: {
