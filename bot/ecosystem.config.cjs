22be4c0cc26e70e24b9465396a738417a504a8d1
venturas55 <venturas55@hotmail.com>
2026-05-08 13:46:12 +0200

    el raiz del proyecto

--- bot/ecosystem.config.cjs
+++ bot/ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "telegrambot",
-      script: "./index.js",
+      script: "./bot/index.js",
       //node_args: "--security-revert=CVE-2023-46809",
       watch: false,
       restart_delay: 3000,
