c496a718c2deed577a2200f3756a5f98a4f7cf30
venturas55 <venturas55@hotmail.com>
2026-06-04 23:48:22 +0200

    mas camaras

--- bot/handlers/commands.js
+++ bot/handlers/commands.js
@@ -39,7 +39,14 @@ export const handleCommands = async (bot, msg) => {
             { text: "🏄‍♂️ Arenas", url: "http://guardiandelfaro.es/cam/arenas.html" }
           ],
           [
+            { text: "🫒 Oliva", url: "https://www.comunitatvalenciana.com/es/valencia/oliva/webcams/oliva-puerto" }
+          ]
+          ,
+          [
             { text: "🌅 Altea", url: "http://guardiandelfaro.es/cam/altea.html" }
+          ],
+          [
+            { text: "🪢 Santa Pola", url: "https://www.comunitatvalenciana.com/es/alacant-alicante/santa-pola/webcams/santa-pola-gran-playa" }
           ]
         ]
       }
